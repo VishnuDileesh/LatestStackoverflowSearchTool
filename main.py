f0c101941759e105c7350051f73935502c5c27b2
Pheyonix <dev@vishnudileesh.com>
2020-04-24 18:00:38 +0530

    Lol done with it

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+from sys import argv
 from helium import *
 
-start_chrome()
+search_site = 'site:stackoverflow.com '
+
+search_phrase = search_site + argv[1]
+
+print(search_phrase)
+
+start_chrome('google.com')
+
+write(search_phrase)
+
+press(ENTER)
+
+click('Tools')
+
+click('Any time')
+
+click('Past year')
