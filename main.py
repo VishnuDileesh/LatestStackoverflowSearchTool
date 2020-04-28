1bfc8786a860c1ff1cad360d2dbfb8c08bde6631
Pheyonix <dev@vishnudileesh.com>
2020-04-28 18:42:41 +0530

    add duckduckgo latest selection feature

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from sys import argv
 from helium import *
 
@@ -5,21 +7,34 @@ search_site = 'site:stackoverflow.com '
 
 search_phrase = search_site + argv[1]
 
-print(search_phrase)
 
 if argv[2] == 'd':
     start_chrome('duckduckgo.com')
+
+    write(search_phrase)
+
+    press(ENTER)
+
+    click('Any Time')
+
+    click('Past Year')
+
+
 elif argv[2] == 'g':
     start_chrome('google.com')
+
+    write(search_phrase)
+
+    press(ENTER)
+
+    click('Tools')
+
+    click('Any time')
+
+    click('Past year')
+
 else:
+
     print('Please enter d or g to choose the search engine')
 
-write(search_phrase)
 
-press(ENTER)
-
-click('Tools')
-
-click('Any time')
-
-click('Past year')
